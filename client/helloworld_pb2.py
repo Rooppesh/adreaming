ef9ba3fc1c409a071fbf5ce083a34252c7642ece
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-03-16 12:43:33 -0700

    size changes

--- client/helloworld_pb2.py
+++ client/helloworld_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10helloworld.proto\x12\nhelloworld\"\x1c\n\x07Request\x12\x11\n\trowOffset\x18\x01 \x01(\x05\"\xb2\x01\n\x03Row\x12\x0e\n\x06tconst\x18\x01 \x01(\t\x12\x11\n\ttitleType\x18\x02 \x01(\t\x12\x14\n\x0cprimaryTitle\x18\x03 \x01(\t\x12\x15\n\roriginalTitle\x18\x04 \x01(\t\x12\x0f\n\x07isAdult\x18\x05 \x01(\x08\x12\x11\n\tstartYear\x18\x06 \x01(\t\x12\x0f\n\x07\x65ndYear\x18\x07 \x01(\t\x12\x16\n\x0eruntimeMinutes\x18\x08 \x01(\t\x12\x0e\n\x06genres\x18\t \x01(\t\"*\n\x06Result\x12 \n\x07results\x18\x01 \x03(\x0b\x32\x0f.helloworld.Row2B\n\x06Getter\x12\x38\n\x0bGetIMDBData\x12\x13.helloworld.Request\x1a\x12.helloworld.Result\"\x00\x42\x36\n\x1bio.grpc.examples.helloworldB\x0fHelloWorldProtoP\x01\xa2\x02\x03HLWb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10helloworld.proto\x12\nhelloworld\"\x1c\n\x07Request\x12\x11\n\trowOffset\x18\x01 \x01(\x05\"\xb2\x01\n\x03Row\x12\x0e\n\x06tconst\x18\x01 \x01(\t\x12\x11\n\ttitleType\x18\x02 \x01(\t\x12\x14\n\x0cprimaryTitle\x18\x03 \x01(\t\x12\x15\n\roriginalTitle\x18\x04 \x01(\t\x12\x0f\n\x07isAdult\x18\x05 \x01(\x08\x12\x11\n\tstartYear\x18\x06 \x01(\t\x12\x0f\n\x07\x65ndYear\x18\x07 \x01(\t\x12\x16\n\x0eruntimeMinutes\x18\x08 \x01(\t\x12\x0e\n\x06genres\x18\t \x01(\t\"8\n\x06Result\x12 \n\x07results\x18\x01 \x03(\x0b\x32\x0f.helloworld.Row\x12\x0c\n\x04size\x18\x02 \x01(\x05\x32\x42\n\x06Getter\x12\x38\n\x0bGetIMDBData\x12\x13.helloworld.Request\x1a\x12.helloworld.Result\"\x00\x42\x36\n\x1bio.grpc.examples.helloworldB\x0fHelloWorldProtoP\x01\xa2\x02\x03HLWb\x06proto3')
 
 
 
@@ -52,7 +52,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _ROW._serialized_start=63
   _ROW._serialized_end=241
   _RESULT._serialized_start=243
-  _RESULT._serialized_end=285
-  _GETTER._serialized_start=287
-  _GETTER._serialized_end=353
+  _RESULT._serialized_end=299
+  _GETTER._serialized_start=301
+  _GETTER._serialized_end=367
 # @@protoc_insertion_point(module_scope)
