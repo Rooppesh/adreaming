65b7a3b09c1c59c0fa0a639ded9fd27adf09d0f2
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-03-17 22:35:54 -0700

    update_client

--- client/client.py
+++ client/client.py
@@ -14,23 +14,44 @@
 """The Python implementation of the GRPC helloworld.Greeter client."""
 
 from __future__ import print_function
+from ctypes import sizeof
 
+import time
 import logging
-
+from flask import before_render_template
+import pandas
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
-
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    # with grpc.insecure_channel('169.254.251.55:50051') as channel:
-    with grpc.insecure_channel('localhost:50051') as channel:
+    row_count = 0
+    itr = 1
+    fileReader = open("../data/received_data.csv", "r")
+    fileAppender = open("../data/received_data.csv", "a")
+    yAxisWriter = open("../data/y_axis.csv", "a")
+
+    with grpc.insecure_channel('169.254.211.61:50051') as channel:
+    # with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GetterStub(channel)
-        response = stub.GetIMDBData(helloworld_pb2.Request(rowOffset=0))
-    print(response.results)
+        while row_count < 4194304: 
+            row_count += int((sum(1 for row in fileReader))/8) 
+            # row_count = int(row_count/8)
+            print("wrote row " + str(row_count))
+            # row_count = int((row_count - (row_count%8))/8)
+            beforeTime = time.time() 
+            response = stub.GetIMDBData(helloworld_pb2.Request(rowOffset=row_count))
+            afterTime = time.time()
+            fileAppender.write(str(response.results))
+            # print("Time elapsed for write " + str(itr) + ": " + str(afterTime-beforeTime))
+            yAxisWriter.write(str(afterTime-beforeTime)+"\n")
+            itr = itr+1
+    
+    fileReader.close()
+    fileAppender.close()
 
 
 if __name__ == '__main__':
