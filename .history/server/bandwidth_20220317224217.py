455969eadffcd30ed7b696e7cb3fe23d38cf5204
abhishekG <abhishek.ganesan@sjsu.edu>
2022-03-17 23:03:21 -0700

    remove .history files

--- .history/server/bandwidth_20220317224217.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-def returnBandwidth():
-    # value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
-    value = psutil.net_io_counters(pernic=False)
-    # bandwidth = value/1024./1024.*8
-    return(value[0]/1024./1024.)
